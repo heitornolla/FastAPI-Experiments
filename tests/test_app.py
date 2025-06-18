de789e654fc004f8d785f3034c54e026a4504520
heitor <heitor.nolla@gmail.com>
2025-06-18 12:28:54 -0300

    new test

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,29 @@ from fastapi.testclient import TestClient
 from fastapi_project.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
+def test_root_hello_world():
     client = TestClient(app)
 
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'email': 'test@example.com',
+            'password': 'secretpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'test',
+        'email': 'test@example.com',
+    }
