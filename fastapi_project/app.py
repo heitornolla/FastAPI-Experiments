63469544f5b500cca9e897eb2564563c104df1dc
heitor <heitor.nolla@gmail.com>
2025-06-18 12:29:03 -0300

    linting

--- fastapi_project/app.py
+++ fastapi_project/app.py
@@ -2,12 +2,13 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_project.schemas import Message, UserPublic, UserSchema, UserDB
+from fastapi_project.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
 # fake in-memory database
 database = []
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
@@ -16,7 +17,7 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database)+1)
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
 
     database.append(user_with_id)
     return user_with_id
