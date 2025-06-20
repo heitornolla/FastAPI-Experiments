0e8a1aeec3fc6385ed5f087f9aa977b8506e5428
heitor <heitor.nolla@gmail.com>
2025-06-19 22:00:27 -0300

    improved test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,11 +1,15 @@
+from sqlalchemy import select
+
 from fastapi_project.models import User
 
 
 def test_create_user(session):
-    user = User(username='test', email='test@example.com', password='secret')
+    new_user = User(username='test', email='test@example.com', password='secret')
 
-    session.add(user)
+    session.add(new_user)
     session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'test'))
 
     assert user.username == 'test'
     assert user.email == 'test@example.com'
