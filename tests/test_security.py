5e2385bccdaf5cefd7210148c979ad2252575c9c
heitor <heitor.nolla@gmail.com>
2025-06-24 18:42:04 -0300

    linting

--- tests/test_security.py
+++ tests/test_security.py
@@ -6,7 +6,7 @@ from fastapi_project.security import create_access_token
 def test_jwt(settings):
     data = {'test': 'test'}
     token = create_access_token(data)
- 
+
     decoded = decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
 
     assert decoded['test'] == data['test']
