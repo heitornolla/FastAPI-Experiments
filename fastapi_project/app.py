34613d540e2bfb9873143b3f44c8ae7d541099db
heitor <heitor.nolla@gmail.com>
2025-06-24 17:43:52 -0300

    refactoring for better structure

--- fastapi_project/app.py
+++ fastapi_project/app.py
@@ -3,23 +3,11 @@ from http import HTTPStatus
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
 
 from fastapi_project.database import get_session
 from fastapi_project.models import User
-from fastapi_project.schemas import (
-    Message,
-    Token,
-    UserList,
-    UserPublic,
-    UserSchema,
-)
-from fastapi_project.security import (
-    create_access_token,
-    get_current_user,
-    get_password_hash,
-    verify_password,
-)
+from fastapi_project.schemas import Message, Token
+from fastapi_project.security import create_access_token, verify_password
 
 app = FastAPI()
 
@@ -27,75 +15,6 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
-
-
-@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def get_users(
-    limit=10, offset=0, session=Depends(get_session), current_user=Depends(get_current_user)
-):
-    users = session.scalars(select(User).offset(offset).limit(limit)).all()
-
-    return {'users': users}
-
-
-@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session=Depends(get_session)):
-    db_register = session.scalar(
-        select(User).where((User.email == user.email) | (User.username == user.username))
-    )
-
-    if db_register:
-        if db_register.username == user.username:
-            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail='Username already exists')
-        elif db_register.email == user.email:
-            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail='Email already exists')
-
-    db_user = User(
-        username=user.username, password=get_password_hash(user.password), email=user.email
-    )
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-
-    return db_user
-
-
-@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
-def update_user(
-    user_id: int,
-    user: UserSchema,
-    session=Depends(get_session),
-    current_user=Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail='Not allowed')
-
-    try:
-        current_user.email = user.email
-        current_user.username = user.username
-        current_user.password = get_password_hash(user.password)
-
-        session.add(current_user)
-        session.commit()
-        session.refresh(current_user)
-
-        return current_user
-
-    except IntegrityError:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT, detail='Username or Email already exists'
-        )
-
-
-@app.delete('/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message)
-def delete_user(user_id: int, session=Depends(get_session), current_user=Depends(get_current_user)):
-    if current_user.id != user_id:
-        raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail='Not allowed')
-
-    session.delete(current_user)
-    session.commit()
-
-    return {'message': 'User deleted'}
 
 
 @app.post('/token/', response_model=Token)
---
+++ fastapi_project/routers/users.py
@@ -0,0 +1,89 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+
+from fastapi_project.database import get_session
+from fastapi_project.models import User
+from fastapi_project.schemas import (
+    Message,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
+from fastapi_project.security import (
+    get_current_user,
+    get_password_hash,
+)
+
+router = APIRouter(prefix='/users' ,tags=['users'])
+
+
+@router.get('/', status_code=HTTPStatus.OK, response_model=UserList)
+def get_users(
+    limit=10, offset=0, session=Depends(get_session), current_user=Depends(get_current_user)
+):
+    users = session.scalars(select(User).offset(offset).limit(limit)).all()
+
+    return {'users': users}
+
+
+@router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema, session=Depends(get_session)):
+    db_register = session.scalar(
+        select(User).where((User.email == user.email) | (User.username == user.username))
+    )
+
+    if db_register:
+        if db_register.username == user.username:
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail='Username already exists')
+        elif db_register.email == user.email:
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail='Email already exists')
+
+    db_user = User(
+        username=user.username, password=get_password_hash(user.password), email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
+
+
+@router.put('/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def update_user(
+    user_id: int,
+    user: UserSchema,
+    session=Depends(get_session),
+    current_user=Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail='Not allowed')
+
+    try:
+        current_user.email = user.email
+        current_user.username = user.username
+        current_user.password = get_password_hash(user.password)
+
+        session.add(current_user)
+        session.commit()
+        session.refresh(current_user)
+
+        return current_user
+
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT, detail='Username or Email already exists'
+        )
+
+
+@router.delete('/{user_id}', status_code=HTTPStatus.OK, response_model=Message)
+def delete_user(user_id: int, session=Depends(get_session), current_user=Depends(get_current_user)):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail='Not allowed')
+
+    session.delete(current_user)
+    session.commit()
+
+    return {'message': 'User deleted'}
